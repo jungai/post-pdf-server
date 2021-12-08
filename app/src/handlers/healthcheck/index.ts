52344aa4282e0a107f06951a2df2adca39563f78
jungai <juworaphol125@gmail.com>
2021-12-08 21:04:22 +0700

    fix: copilot auto for me lol

--- app/src/handlers/healthcheck/index.ts
+++ app/src/handlers/healthcheck/index.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify";
 
 export const healthCheck = (fast: FastifyInstance["get"], path: string) => {
-    fast("/healthcheck", (_req, reply) => {
+    fast(path, (_req, reply) => {
         reply.send({ msg: "good" });
     });
 };
