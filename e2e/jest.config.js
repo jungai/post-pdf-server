5a89ec2ae386335cc4e2d7860746ded1715c488a
jungai <juworaphol125@gmail.com>
2021-12-08 21:00:22 +0700

    style: lint happy

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig-spec.json'
-    }
-  }
+    "ts-jest": {
+      tsconfig: "tsconfig-spec.json",
+    },
+  },
 };
